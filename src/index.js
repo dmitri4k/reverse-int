c13e795fd463dcd95e7cb818c261752e43d10b07
dmitri4k <mdsunknown@gmail.com>
2023-01-03 20:43:56 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = String(n);
+    let arr = Array.from(str);
+    arr.reverse();
+    let strReversed = arr.join("");
+    let result = parseInt(strReversed);
+    return result;
+};
+
